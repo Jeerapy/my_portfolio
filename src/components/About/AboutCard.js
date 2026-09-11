97fd0150abb9ac64358b8535f1ea302f5eee7467
Yashraj Nair <nairyashraj01@gmail.com>
2026-09-11 13:45:38 +0530

    new

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,15 +8,14 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi everyone! I’m <span className="purple">Soumyajit Behera</span>{" "}
-            from <span className="purple">Bhubaneswar, India</span>.
+            Hi everyone! I'm <span className="purple">Yashraj Nair</span>{" "}
+            from <span className="purple">Chennai, India</span>.
             <br />
-            I’m currently working as a{" "}
-            <span className="purple">Software Developer</span> at{" "}
-            <span className="purple">Juspay</span>.
-            <br />I hold an Integrated M.Sc. (IMSc) in{" "}
-            <span className="purple">Mathematics and Computing</span> from{" "}
-            <span className="purple">BIT Mesra</span>.
+            I'm currently a student at{" "}
+            <span className="purple">Reva University</span>.
+            <br />
+            I'm pursuing a degree in{" "}
+            <span className="purple">B.Tech Computer Science and System Engineering</span>.
             <br />
             <br />
             Outside of coding, I love engaging in activities that keep me
@@ -28,17 +27,11 @@ function AboutCard() {
               <ImPointRight /> Playing Games 🎮
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs ✍️
-            </li>
-            <li className="about-activity">
               <ImPointRight /> Traveling and Exploring New Places 🌍
             </li>
           </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
-          </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+
         </blockquote>
       </Card.Body>
     </Card>
